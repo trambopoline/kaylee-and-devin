a37a45fd361e5fd53b3fb7aaa4df8501419aee4b
Devin Dodson <ddodson5.work@gmail.com>
2018-11-12 17:33:43 -0800

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    `gatsby-plugin-favicon`,
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
